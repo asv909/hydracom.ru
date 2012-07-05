4da412dc75fed97add0e2af31d5f20a3c630595c
asv <asv@asv-GS>
2012-07-05 12:00:37 +0400

    Authenticate managers feature was implemented + code is clear + self layout for login page

--- protected/modules/service/views/layouts/login.php
+++ protected/modules/service/views/layouts/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>HYDRACOM - !электронный гипермаркет промышленных комплектующих!</title>
+        <title>HYDRACOM - авторизация менеджеров</title>
         <meta charset="utf-8">
     </head>
     <body>
