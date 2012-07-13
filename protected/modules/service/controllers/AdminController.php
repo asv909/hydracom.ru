0dfbca592a3b9f68a9016bade38d38c648a5fe9b
asv <asv@asv-GS>
2012-07-13 14:45:37 +0400

    AdminPanel beginning

---
+++ protected/modules/service/controllers/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * AdminController class file 
+ * 
+ * @author Sergey Alekseev <asv909@gmail.com>
+ * @link http://www.eurotrade-et.ru/
+ * @copyright Copyright &#169; 2012 RGK LLC
+ */
+
+/**
+ * The <var>AdminController</var> is .
+ * 
+ * @author Sergey Alekseev <asv909@gmail.com>
+ * @version $Id: AdminController.php v 1.0 2012-07-12 12:00:00 asv909 $
+ * @package HYDRACOM application.modules.service.
+ * @since 1.0
+ */
+class AdminController extends ServiceController {
+    //put your code here
+}
